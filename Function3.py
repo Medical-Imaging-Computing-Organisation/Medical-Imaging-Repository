d2f29e61e5b98f9e9238d9662dc55a43eb325f7e
Adam Andrews <156348212+aga165@users.noreply.github.com>
2024-02-01 19:01:41 +0000

    Moved non-function to name==main

--- Function3.py
+++ Function3.py
@@ -31,12 +31,13 @@ def PutEmTogether(f1, f2):
     f3[:, 2:] = f2[idx, 0:29]  # rest of row
     return f3
 
-f1 = np.ones((4, 4))
-f2 = np.ones((5, 31))
-f2[0, 29:31] = [0, 0]   
-f2[1, 29:31] = [0, 1]
-f2[2, 29:31] = [1, 0] 
-f2[3, 29:31] = [1, 1]
-f2[4, 29:31] = [0, 2]
-f3result = PutEmTogether(f1, f2)
-# print(f3result[0])
+if __name__ == "__main__":
+    f1 = np.ones((4, 4))
+    f2 = np.ones((5, 31))
+    f2[0, 29:31] = [0, 0]
+    f2[1, 29:31] = [0, 1]
+    f2[2, 29:31] = [1, 0]
+    f2[3, 29:31] = [1, 1]
+    f2[4, 29:31] = [0, 2]
+    f3result = PutEmTogether(f1, f2)
+    # print(f3result[0])
