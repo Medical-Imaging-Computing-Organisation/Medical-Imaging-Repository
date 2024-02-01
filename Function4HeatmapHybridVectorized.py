c90d5056ab46b0d319cf64908b37be5d61d1a811
Adam Andrews <156348212+aga165@users.noreply.github.com>
2024-02-01 18:57:27 +0000

    Moved non-function parts to name == main

--- Function4HeatmapHybridVectorized.py
+++ Function4HeatmapHybridVectorized.py
@@ -12,10 +12,6 @@ from matplotlib.colors import LinearSegmentedColormap as LSC
 # import psutil
 # import os
 
-try:
-    plt.close()
-finally:
-    pass
 
 '''
 Function 4 (Richard) Generate set of points corresponding to cones: 
@@ -82,32 +78,6 @@ def cones_generator(a, p, xmin, xmax, ymin, ymax, Lmax=2.5):
     # lmu = process.memory_info().rss - base_memory_usage
     # print(f'memory used {lmu}')
     return vector
-#test data
-N = 1000
-theta = np.pi*np.linspace(20,50, N)/180
-x1 = np.linspace(0,0, N)
-y1 = np.linspace(-2,-2, N)
-z1 = np.linspace(0,0, N)
-null = np.linspace(0,0,N)
-one = np.linspace(1,1,N)
-T = np.linspace(0, np.pi/2, N)
-R11, R12, R13 = one, null, null
-R21, R22, R23 = null, one*np.cos(T), one*np.sin(T)
-R31, R32, R33 = null, one*np.sin(T), one*np.cos(T)
-array_in_test = np.zeros((N,32))
-array_in_test[:,0] = theta
-array_in_test[:,14] = R11
-array_in_test[:,15] = R12
-array_in_test[:,16] = R13
-array_in_test[:,17] = R21
-array_in_test[:,18] = R22
-array_in_test[:,19] = R23
-array_in_test[:,20] = R31
-array_in_test[:,21] = R32
-array_in_test[:,22] = R33
-array_in_test[:,2] = x1
-array_in_test[:,3] = y1
-array_in_test[:,4] = z1
 
 
 ''' Building voxel grid '''
@@ -121,7 +91,7 @@ def build_voxels(dnsy=51, lim=2.5):
     data = np.zeros((dnsy, dnsy, dnsy))  # empty dataset
     return h, v, d, data, voxel_r, dnsy, lim
 
-h, v, d, data, voxel_r, dnsy, lim = build_voxels(51, 2.5)
+
 
 def voxel_fit(h, v, d, xyz, shape, voxel_r):
     '''Fitting into voxels'''
@@ -133,52 +103,86 @@ def voxel_fit(h, v, d, xyz, shape, voxel_r):
     # adds 1 to every voxel specified, including duplicate indices
     return data1
 
+if __name__ == "__main__":
 
-# process = psutil.Process(os.getpid())
-# base_memory_usage = process.memory_info().rss
-# start = timer()
-points = cones_generator(array_in_test, 100, -2.5, 2.5, -2.5, 2.5)
-data = voxel_fit(h, v, d, points, data.shape, voxel_r)
+    try:
+        plt.close()
+    finally:
+        pass
+    
+    #test data
+    N = 1000
+    theta = np.pi*np.linspace(20,50, N)/180
+    x1 = np.linspace(0,0, N)
+    y1 = np.linspace(-2,-2, N)
+    z1 = np.linspace(0,0, N)
+    null = np.linspace(0,0,N)
+    one = np.linspace(1,1,N)
+    T = np.linspace(0, np.pi/2, N)
+    R11, R12, R13 = one, null, null
+    R21, R22, R23 = null, one*np.cos(T), one*np.sin(T)
+    R31, R32, R33 = null, one*np.sin(T), one*np.cos(T)
+    array_in_test = np.zeros((N,32))
+    array_in_test[:,0] = theta
+    array_in_test[:,14] = R11
+    array_in_test[:,15] = R12
+    array_in_test[:,16] = R13
+    array_in_test[:,17] = R21
+    array_in_test[:,18] = R22
+    array_in_test[:,19] = R23
+    array_in_test[:,20] = R31
+    array_in_test[:,21] = R32
+    array_in_test[:,22] = R33
+    array_in_test[:,2] = x1
+    array_in_test[:,3] = y1
+    array_in_test[:,4] = z1
 
-# for n in np.linspace(0, N, 20):
-#     points = cones_generator(array_in_test[int(n):int(n+N/20)], 100, -2.5, 2.5, -2.5, 2.5)
-#     data += voxel_fit(h, v, d, points)
-# print(f"{timer()-start}", "seconds")
-# lmu = process.memory_info().rss - base_memory_usage
-# print(f'memory used {lmu}')
+    h, v, d, data, voxel_r, dnsy, lim = build_voxels(51, 2.5)
+    # process = psutil.Process(os.getpid())
+    # base_memory_usage = process.memory_info().rss
+    # start = timer()
+    points = cones_generator(array_in_test, 100, -2.5, 2.5, -2.5, 2.5)
+    data = voxel_fit(h, v, d, points, data.shape, voxel_r)
+
+    # for n in np.linspace(0, N, 20):
+    #     points = cones_generator(array_in_test[int(n):int(n+N/20)], 100, -2.5, 2.5, -2.5, 2.5)
+    #     data += voxel_fit(h, v, d, points)
+    # print(f"{timer()-start}", "seconds")
+    # lmu = process.memory_info().rss - base_memory_usage
+    # print(f'memory used {lmu}')
 
 
-''' Drawing all that '''
-fig, ax = plt.subplot_mosaic([[1, 1, 2], [1, 1, 3], [1, 1, 4]], figsize=(10, 5),
-             per_subplot_kw={1: {'projection': '3d', 'xlabel': 'x', 'ylabel': 'y', 'zlabel': 'z'},
-                             2: {'aspect': 'equal', 'xlabel': 'x', 'ylabel': 'z'},
-                             3: {'aspect': 'equal', 'xlabel': 'y', 'ylabel': 'z'},
-                             4: {'aspect': 'equal', 'xlabel': 'y', 'ylabel': 'x'}})
+    ''' Drawing all that '''
+    fig, ax = plt.subplot_mosaic([[1, 1, 2], [1, 1, 3], [1, 1, 4]], figsize=(10, 5),
+                 per_subplot_kw={1: {'projection': '3d', 'xlabel': 'x', 'ylabel': 'y', 'zlabel': 'z'},
+                                 2: {'aspect': 'equal', 'xlabel': 'x', 'ylabel': 'z'},
+                                 3: {'aspect': 'equal', 'xlabel': 'y', 'ylabel': 'z'},
+                                 4: {'aspect': 'equal', 'xlabel': 'y', 'ylabel': 'x'}})
 
-try:  # Colour map creation in try to prevent recreation error
-    color_array = plt.get_cmap('YlOrRd')(range(256))
-    color_array[:, -1] = np.linspace(0.0, 1.0, 256)
-    map_object = LSC.from_list(name='YlOrRd_alpha2', colors=color_array)
-    plt.colormaps.register(cmap=map_object)
-except ValueError:
-    pass
+    try:  # Colour map creation in try to prevent recreation error
+        color_array = plt.get_cmap('YlOrRd')(range(256))
+        color_array[:, -1] = np.linspace(0.0, 1.0, 256)
+        map_object = LSC.from_list(name='YlOrRd_alpha2', colors=color_array)
+        plt.colormaps.register(cmap=map_object)
+    except ValueError:
+        pass
 
-XYZ = ax[1].scatter(h, v, d, marker='s', s=2000/dnsy, c=data, cmap="YlOrRd_alpha2")
-plt.colorbar(XYZ, location='left')
+    XYZ = ax[1].scatter(h, v, d, marker='s', s=2000/dnsy, c=data, cmap="YlOrRd_alpha2")
+    plt.colorbar(XYZ, location='left')
 
 
-mid = int((dnsy-1)/2)
-var = int(mid/5)
-# XZ = ax[2].pcolormesh(h[0], d[0], np.sum(data, axis=0), cmap="YlOrRd")
-XZ = ax[2].pcolormesh(h[0], d[0], np.sum(data[mid-var:mid+var+1, :, :], axis=0), cmap="YlOrRd")
-cb2 = plt.colorbar(XZ)  # X-Z and Y-Z colour maps
-# YZ = ax[3].pcolormesh(h[0], d[0], np.sum(data, axis=1), cmap="YlOrRd")
-YZ = ax[3].pcolormesh(h[0], d[0], np.sum(data[:, mid-var:mid+var+1, :], axis=1), cmap="YlOrRd")
-cb3 = plt.colorbar(YZ)
-XY = ax[4].pcolormesh(h[0], d[0], np.sum(data[:, :, mid-var:mid+var+1], axis=2), cmap="YlOrRd")
-cb4 = plt.colorbar(XY)
+    mid = int((dnsy-1)/2)
+    var = int(mid/5)
+    # XZ = ax[2].pcolormesh(h[0], d[0], np.sum(data, axis=0), cmap="YlOrRd")
+    XZ = ax[2].pcolormesh(h[0], d[0], np.sum(data[mid-var:mid+var+1, :, :], axis=0), cmap="YlOrRd")
+    cb2 = plt.colorbar(XZ)  # X-Z and Y-Z colour maps
+    # YZ = ax[3].pcolormesh(h[0], d[0], np.sum(data, axis=1), cmap="YlOrRd")
+    YZ = ax[3].pcolormesh(h[0], d[0], np.sum(data[:, mid-var:mid+var+1, :], axis=1), cmap="YlOrRd")
+    cb3 = plt.colorbar(YZ)
+    XY = ax[4].pcolormesh(h[0], d[0], np.sum(data[:, :, mid-var:mid+var+1], axis=2), cmap="YlOrRd")
+    cb4 = plt.colorbar(XY)
 
-ax[1].set_title('3D Graph')
-ax[1].set_zlim(-lim, lim)
-plt.tight_layout()
-plt.show()
+    ax[1].set_title('3D Graph')
+    ax[1].set_zlim(-lim, lim)
+    plt.tight_layout()
+    plt.show()
