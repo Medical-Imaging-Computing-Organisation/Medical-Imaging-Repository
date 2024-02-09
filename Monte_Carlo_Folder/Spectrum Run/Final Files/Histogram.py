7dab4052c9d1bbe1cfb158d127544f9e6fb124dc
Euan Walsh <156348291+Baggie270@users.noreply.github.com>
2024-02-09 12:27:19 +0000

    Add files via upload

---
+++ Monte_Carlo_Folder/Spectrum Run/Final Files/Histogram.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import csv
+data = []
+with open(r"C:\Users\euan6\Downloads\temp\Energydeps.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        for i in row:
+            data.append(float(i))
+
+data = sorted(data)
+numberlist = sorted(list(set(data)))
+# use to get rid of 622 peak
+# numberlist = numberlist[:-1]
+energy_dep = {}
+for i in data:
+    if i not in energy_dep:
+        energy_dep[i] = 1
+    else:
+        energy_dep[i] += 1
+countlist = list(energy_dep.values())
+# use to get rid of 0s and 622 peak
+# countlist = countlist[:-1]
+print(countlist)
+print(numberlist)
+
+plt.figure('Histogram of Energy Depositions')
+plt.xlabel('Energy deposition (Mev)')
+plt.ylabel('Count')
+plt.title('Number Of Times Energy Is Deposited')
+plt.plot(numberlist, countlist, color='k')
